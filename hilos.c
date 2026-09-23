44a0e7cbea7b6b8e94eab8e6cc18bcb172ae6efb
lfcondoric <lfcondoric@unjbg.edu.pe>
2026-09-23 09:52:17 -0500

    Agregando funcion pthread_mutex_t mutex

--- hilos.c
+++ hilos.c
@@ -7,6 +7,15 @@ void* hilo_funcion(void* arg){
 	pthread_exit(NULL);
 }
 
+pthread_mutex_t mutex;
+
+void* hilo_funcion_con_mutex(void* arg){
+	pthread_mutex_lock(&mnutex);
+	printf("Hola desde el hilo &ld\n",(long)arg);
+	pthread_mutex_unlock(&mutex);
+	pthread.exit(NULL);
+}
+
 int main(){
 	pthread_t hilo1, hilo2;
 
